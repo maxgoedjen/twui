0d537c2770734490d6426ba2ea65bb78a7c69fc3
Max Goedjen <max.goedjen@gmail.com>
2012-11-13 20:48:15 -0800

    Partial fix for #113.
    Doesn't totally fix it, header doesn't properly move in some cases
    (rows near the header).

--- lib/UIKit/TUITableView.m
+++ lib/UIKit/TUITableView.m
@@ -772,8 +772,10 @@ static NSInteger SortCells(TUITableViewCell *a, TUITableViewCell *b, void *ctx)
 					}
 				}
 				
-				section.headerView.frame = headerFrame;
-				[section.headerView setNeedsLayout];
+				if (![self __isDraggingCell]) {
+					section.headerView.frame = headerFrame;
+					[section.headerView setNeedsLayout];
+				}
 				
 				if(section.headerView.superview == nil){
 					[self addSubview:section.headerView];
